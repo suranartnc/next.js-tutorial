a8bbbc2de8dbebff370a2f2670623f5205246c6e
SuranartN <suranartnc@gmail.com>
2017-12-10 23:24:19 +0700

    Fetch data for entry page

--- pages/entry.js
+++ pages/entry.js
@@ -1,16 +1,34 @@
+require('isomorphic-fetch')
+
 import React from 'react'
 import { Link } from '../routes'
 
-export default function HomePage(props) {
-  return (
-    <div>
-      <p>Entry Page</p>
-      <p>ID: {props.url.query.id}</p>
-      <p>
-        <Link route="home">
-          <a>Go to Home page</a>
-        </Link>
-      </p>
-    </div>
-  )
+export default class HomePage extends React.Component {
+  static getInitialProps(context) {
+    return fetch(`http://localhost:4000/posts/${context.query.id}`)
+      .then(res => res.json())
+      .then(json => {
+        return {
+          entry: json
+        }
+      })
+  }
+
+  render() {
+    const { title, body } = this.props.entry
+
+    return (
+      <div>
+        <p>
+          <Link route="home">
+            <a>Go to Home page</a>
+          </Link>
+        </p>
+        <article>
+          <h1>{title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: body }} />
+        </article>
+      </div>
+    )
+  }
 }

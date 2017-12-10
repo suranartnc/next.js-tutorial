a0810c1f5b11bd3afd86fb96d9eefe83f859a570
SuranartN <suranartnc@gmail.com>
2017-12-10 22:05:21 +0700

    Add links

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
 export default function AboutPage() {
-  return <div>About Page</div>
+  return (
+    <div>
+      <p>About Page</p>
+      <Link href="/">
+        <a>Go to Home page</a>
+      </Link>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
 export default function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <div>
+      <p>Home Page</p>
+      <Link href="/about">
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  )
 }

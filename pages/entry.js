ee8d01903c424be4ed35fe5d9646ed63127dceaf
SuranartN <suranartnc@gmail.com>
2017-12-11 13:17:32 +0700

    Adopt async/await

--- pages/entry.js
+++ pages/entry.js
@@ -7,20 +7,19 @@ import fetchAPI from '../utils/fetchAPI'
 import MainLayout from '../components/layouts/MainLayout'
 
 export default class EntryPage extends React.Component {
-  static getInitialProps(context) {
+  static async getInitialProps(context) {
     const entryPromise = fetchAPI(`/posts/${context.query.id}`)
     const relateEntriesPromise = fetchAPI(`/posts`)
 
-    return Promise.all([entryPromise, relateEntriesPromise]).then(function(
-      results
-    ) {
-      const [entryResult, relateEntriesResult] = results
+    const [entryResult, relateEntriesResult] = await Promise.all([
+      entryPromise,
+      relateEntriesPromise
+    ])
 
-      return {
-        entry: entryResult.data,
-        relateEntries: relateEntriesResult.data
-      }
-    })
+    return {
+      entry: entryResult.data,
+      relateEntries: relateEntriesResult.data
+    }
   }
 
   render() {

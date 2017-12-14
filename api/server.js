76d1f95474ada87cb858d5495d058bc1c5ea6ab8
Suranart Niamcome <suranartnc@gmail.com>
2017-12-14 10:06:28 +0700

    Add delay to api

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ var jsonServer = require('json-server')
 var db = require('./db')
 
 var port = 4000
+var delay = 500
 
 var server = jsonServer.create()
 var router = jsonServer.router(db())
 
 server.use(jsonServer.defaults())
+server.use((req, res, next) => {
+  setTimeout(function() {
+    next()
+  }, delay)
+})
 server.use(router)
 
 server.listen(port)

f03292c5cedcc805d3907fa6aa18d4611019fd9b
Suranart Niamcome <suranartnc@gmail.com>
2017-12-14 10:04:22 +0700

    Add layout

--- pages/entry.js
+++ pages/entry.js
@@ -1,16 +1,36 @@
+import 'isomorphic-fetch'
+
 import React from 'react'
 import { Link } from '../routes'
 
-export default function HomePage(props) {
+import MainLayout from '../components/layouts/MainLayout'
+
+function EntryPage({ entry }) {
   return (
-    <div>
-      <p>Entry Page</p>
-      <p>ID: {props.url.query.id}</p>
+    <MainLayout>
+      <h1>{entry.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: entry.body }} />
       <p>
         <Link route="home">
           <a>Go to Home page</a>
         </Link>
       </p>
-    </div>
+    </MainLayout>
   )
+}
+
+export default class EntryPageContainer extends React.Component {
+  static getInitialProps(context) {
+    return fetch(`http://localhost:4000/posts/${context.query.id}`)
+      .then(res => res.json())
+      .then(json => {
+        return {
+          entry: json
+        }
+      })
+  }
+
+  render() {
+    return <EntryPage entry={this.props.entry} />
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
+import 'isomorphic-fetch'
+
 import React from 'react'
 import { Link } from '../routes'
 
-export default function HomePage() {
+import MainLayout from '../components/layouts/MainLayout'
+
+function HomePage({ entries }) {
   return (
-    <div>
-      <p>Home Page</p>
-      <p>
-        <Link route="about">
-          <a>Go to About page</a>
-        </Link>
-      </p>
-      <p>
-        <Link route="entry" params={{ id: 1234 }}>
-          <a>Go to Entry: 1234</a>
-        </Link>
-      </p>
-    </div>
+    <MainLayout>
+      {entries.map(function(entry) {
+        return (
+          <h2 key={entry.id}>
+            <Link route="entry" params={{ id: entry.id }}>
+              <a>{entry.title}</a>
+            </Link>
+          </h2>
+        )
+      })}
+    </MainLayout>
   )
+}
+
+export default class HomePageContainer extends React.Component {
+  static getInitialProps() {
+    return fetch(`http://localhost:4000/posts/`)
+      .then(res => res.json())
+      .then(json => {
+        return {
+          entries: json
+        }
+      })
+  }
+  render() {
+    return <HomePage entries={this.props.entries} />
+  }
 }

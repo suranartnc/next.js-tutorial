a8a8c86437d1eaf96287af890d703c9daddd9a0b
SuranartN <suranartnc@gmail.com>
2017-12-17 21:12:27 +0700

    Add cors to GraphQL server

--- graphql/server.js
+++ graphql/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 
 import { schema, getRootValue, getContext } from './schema'
 
 const PORT = 5000
 const server = express()
+
+server.use(cors())
 
 server.use(
   '/graphql',

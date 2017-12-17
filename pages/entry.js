35671c8db04c194e0bc99407281d52369bf707c5
SuranartN <suranartnc@gmail.com>
2017-12-17 21:31:01 +0700

    Fetch data for entry page

--- pages/entry.js
+++ pages/entry.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 
-import fetchAPI from '../utils/fetchAPI'
+import fetchGQL from '../utils/fetchGQL'
 import { Link } from '../routes'
 
 import MainLayout from '../components/layouts/MainLayout'
 
-function EntryPage({ entry, relateEntries }) {
+function EntryPage({ entry }) {
   return (
     <MainLayout>
       <Head>
@@ -15,7 +15,7 @@ function EntryPage({ entry, relateEntries }) {
       <h1>{entry.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: entry.body }} />
       <div>
-        {relateEntries.map(function(entry) {
+        {entry.relateEntries.map(function(entry) {
           return (
             <h2 key={entry.id}>
               <Link route="entry" params={{ id: entry.id }}>
@@ -31,35 +31,26 @@ function EntryPage({ entry, relateEntries }) {
 
 export default class EntryPageContainer extends React.Component {
   static async getInitialProps(context) {
-    return Promise.all([
-      fetchAPI(`/posts/${context.query.id}`, 'entry'),
-      fetchAPI('/posts/', 'relateEntries')
-    ]).then(([{ entry }, { relateEntries }]) => ({ entry, relateEntries }))
-
-    // Optional #1 - Promise.all() with reduce()
-    // return Promise.all([
-    //   fetchAPI(`/posts/${context.query.id}`, 'entry'),
-    //   fetchAPI('/posts/', 'relateEntries')
-    // ]).then(results => results.reduce((prev, cur) => ({ ...prev, ...cur }), {}))
-
-    // Optional #2 - Async / Await
-    // return (await Promise.all([
-    //   fetchAPI(`/posts/${context.query.id}`, 'entry'),
-    //   fetchAPI('/posts/', 'relateEntries')
-    // ])).reduce(function(previousValue, currentValue) {
-    //   return {
-    //     ...previousValue,
-    //     ...currentValue
-    //   }
-    // }, {})
+    const query = `
+      {
+        post(id: ${context.query.id}) {
+          id
+          title
+          body
+          relateEntries {
+            id
+            title
+          }
+        }
+      }
+    `
+    const { data } = await fetchGQL(query)
+    return {
+      entry: data.post
+    }
   }
 
   render() {
-    return (
-      <EntryPage
-        entry={this.props.entry}
-        relateEntries={this.props.relateEntries}
-      />
-    )
+    return <EntryPage entry={this.props.entry} />
   }
 }

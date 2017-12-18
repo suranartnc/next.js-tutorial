2b76ae390ac9180d7540aa342432e120c1f19255
Suranart Niamcome <suranartnc@gmail.com>
2017-12-18 16:09:30 +0700

    Query using apollo instead

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import 'isomorphic-fetch'
 
 import React from 'react'
+import { compose } from 'recompose'
 import Head from 'next/head'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import page from '../hocs/page'
 
-import fetchGQL from '../utils/fetchGQL'
 import { Link } from '../routes'
 
-function HomePage({ entries }) {
+function HomePage({ data }) {
+  if (data.loading) return 'Loading...'
+
   return (
     <div>
       <Head>
         <title>Next.js Tutorial</title>
       </Head>
       <div>
-        {entries.map(function(entry) {
+        {data.posts.map(function(entry) {
           return (
             <h2 key={entry.id}>
               <Link route="entry" params={{ id: entry.id }}>
@@ -29,24 +33,22 @@ function HomePage({ entries }) {
   )
 }
 
-class HomePageContainer extends React.Component {
-  static async getInitialProps() {
-    const query = `
-      query($limit: Int!) { 
-        posts(limit: $limit) { 
-          id 
-          title
-        } 
-      }
-    `
-    const { data } = await fetchGQL(query, {
-      limit: 20
-    })
-    return { entries: data.posts }
+const QUERY = gql`
+  query($limit: Int!) {
+    posts(limit: $limit) {
+      id
+      title
+    }
   }
-  render() {
-    return <HomePage entries={this.props.entries} />
-  }
-}
+`
 
-export default page(HomePageContainer)
+export default compose(
+  page,
+  graphql(QUERY, {
+    options: () => ({
+      variables: {
+        limit: 20
+      }
+    })
+  })
+)(HomePage)
